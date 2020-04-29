047de8c9aa75b0ae118a71945be8956c8e2ab965
aviralgupta25 <58987503+aviralgupta25@users.noreply.github.com>
2020-04-30 01:00:45 +0530

    Initial Author Route

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-//const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
-
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -12,12 +11,13 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 //app.use(methodOverride('_method'))
 app.use(express.static('public'))
-//app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 const mongoose = require('mongoose')
 
 mongoose.connect("mongodb://localhost:27017/mybrary",{useNewUrlParser:true, useUnifiedTopology: true});
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
